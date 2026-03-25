4a8eebd90b0b15f66c8d56a625cc6abe0694880a
Chubie <chubie2@live.com>
2026-03-25 22:17:07 +0000

    computed variance

--- Day-1/main.py
+++ Day-1/main.py
@@ -1,13 +1,24 @@
 import random 
 
 freq = {i: 0 for i in range(2, 13)}
-ev = 0;
-for i in range(100000):
+trials = 10000000
+ev = 0
+variance = 0
+
+for i in range(trials):
     roll = random.randint(1, 6) + random.randint(1, 6)
     freq[roll] += 1
 
 for total in sorted(freq):
-    prob = freq[total] / 100000
+    prob = freq[total] / trials
+    # For each possible total, we calculate the ev contribution
+    # by multiplying its occrence by the probability of that total occurring
     ev += total * prob
 
+    # For each possible total, we calculate the variance contribution
+    # by multiplying the squared difference from the expected value by 
+    # the probability of that total occurring
+
+    variance += (total - ev) ** 2 * prob
 print("Expected value of the sum of two dice rolls:", ev)
+print("Variance of the sum of two dice rolls:", variance)
