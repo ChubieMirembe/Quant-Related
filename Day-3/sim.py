9fdd53e3a86217a1d086d71270cf99c7a876a7d4
Chubie <71077124+ChubieMirembe@users.noreply.github.com>
2026-03-29 16:24:50 +0100

    Enhance correlation calculation with binning
    Added bins and bins_count dictionaries to categorize y values based on x ranges. Updated correlation calculation and added print statements for bin averages.

--- Day-3/sim.py
+++ Day-3/sim.py
@@ -8,16 +8,35 @@ y2_sum = 0
 xy_sum = 0
 cov = 0
 
+bins = {0.2: 0, 0.4: 0, 0.6: 0, 0.8: 0, 1.0: 0}
+bins_count = {0.2: 0, 0.4: 0, 0.6: 0, 0.8: 0, 1.0: 0}
+
 for _ in range(trials):
     x = random.random()
     # y is a function of x plus some random noise
     # when noise in increase the correlation will decrease
-    y = x + random.random() + random.random() + random.random() + random.random()
+    y = x + random.random() + random.random() + random.random()
     xy_sum += x * y
     x_sum += x
     y_sum += y
     x2_sum += x ** 2
     y2_sum += y ** 2
+    
+    if(x <= 0.2):
+        bins[0.2] += y
+        bins_count[0.2] += 1
+    elif(x <= 0.4):
+        bins[0.4] += y
+        bins_count[0.4] += 1
+    elif(x <= 0.6):
+        bins[0.6] += y
+        bins_count[0.6] += 1
+    elif(x <= 0.8):
+        bins[0.8] += y
+        bins_count[0.8] += 1
+    elif(x <= 1.0):
+        bins[1.0] += y
+        bins_count[1.0] += 1
 
 
 x_sum /= trials
@@ -25,20 +44,14 @@ y_sum /= trials
 xy_sum /= trials
 x2_sum /= trials
 y2_sum /= trials
-
-print("X: ", x_sum) 
-print("Y: ", y_sum) 
-print("XY: ", xy_sum) 
-cov = xy_sum - (x_sum * y_sum)
-print("Cov: ", cov)    
 VarX = x2_sum - (x_sum ** 2)
 VarY = y2_sum - (y_sum ** 2)
-print("Var(X): ", VarX)
-print("Var(Y): ", VarY)
-
+cov = xy_sum - (x_sum * y_sum)
 corr = cov / ((VarX * VarY) ** 0.5)
-print("Corr: ", corr)
 
+for _ in bins:
+    bins[_] = bins[_] / bins_count[_]
+    print(_, " ", bins[_])
 ## When noise is increase the correlation will decrease
 # covariance and correlation are used to detect signal in the presence of noise.
 #  When noise is increase the correlation will decrease.
